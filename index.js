b23b048cc2c6d9b2947b37df8bf7e6057e744116
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2018-08-15 12:48:03 +0530

    Create graphql server wth apollo

---
+++ index.js
@@ -0,0 +1,37 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling s"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen().then(({url}) => {
+  console.log(`🚀  Server ready at ${url}`);
+})
